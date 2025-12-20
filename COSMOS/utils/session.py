e9dc3bab3f810d004ce06ba44c297656ee606ab0
suvadeeproy <82468946+suvadeeproy@users.noreply.github.com>
2025-12-20 09:21:53 -0600

    Delete COSMOS directory

--- COSMOS/utils/session.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-def put_metric(scope: str, key: str, value):
-    st.session_state.setdefault("metrics", {}).setdefault(scope, {})[key] = value
-def get_metrics():
-    return st.session_state.get("metrics", {})
